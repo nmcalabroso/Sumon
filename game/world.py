c3d3c401d9feb8dec3669b85fa777057ef5fc7a2
Mireya Andres <mireyagenandres@gmail.com>
2014-03-17 11:45:58 +0800

    Adds commands to program files. TO DO: prompt user input for board positions.

--- game/world.py
+++ game/world.py
@@ -260,34 +260,41 @@ class GameWorld(GameObject):
 		mana.text = player.get_mana_label()
 			
 	def update(self,dt): #game logic loop
+		# Tranisition states are for setting up the next state. SETUP is also a transition state.
+		# Use boolean self.start_round to execute once during any state.
+		# Player input should be in their respective classes.
+		# (e.g what to do when clicking a card should be seen in cards.py)
+
 		if self.game_state == Resources.state['SETUP']:
+			self.start_round = True
 			self.game_state = Resources.state['TRANSITION_PLAYER1']
 
 		elif self.game_state == Resources.state['TRANSITION_PLAYER1']:
 			if self.start_round:
-				
 				self.round += 1
 				self.start_round = False
 				self.generate_cards('Player1')
 				self.change_player()
 				self.game_state = Resources.state['PLAYER1']
-	
+				self.player_program = open("player1_program.txt", "w")
 
 		elif self.game_state == Resources.state['PLAYER1']:
 			self.start_round = True
 
 		elif self.game_state == Resources.state['TRANSITION_PLAYER2']:
 			if self.start_round:
+				self.player_program.close()
 				self.start_round = False
 				self.generate_cards('Player2')
 				self.change_player()
 				self.game_state = Resources.state['PLAYER2']
+				self.player_program = open("player2_program.txt", "w")
 	
 		elif self.game_state == Resources.state['PLAYER2']:
 			self.start_round = True
 
 		elif self.game_state == Resources.state['TRANSITION_BOARD']:
-			self.game_state = Resources.state['BOARD']
+			self.player_program.close()
 			player1 = self.find_game_object('Player1')
 			player2 = self.find_game_object('Player2')
 			player1.deactivate()
@@ -302,10 +309,54 @@ class GameWorld(GameObject):
 			name.text = ''
 			lives.text = ''
 			mana.text = ''
+			self.game_state = Resources.state['BOARD']
+
+		elif self.game_state == Resources.state['BOARD']:
+			if self.start_round:
+				self.start_round = False
+				program1 = []
+				program2 = []
+
+				#get commands from both programs
+				player_program1 = open("player1_program.txt", "r")
+				player_program2 = open("player2_program.txt", "r")
+				program1 = player_program1.readlines()
+				program2 = player_program2.readlines()
+				player_program1.close()
+				player_program2.close()
+
+				#get max number of commands
+				if len(program1) > len(program2):
+					max = len(program1)
+				else:
+					max = len(program2)
+
+				#for each command
+				for i in range(max):
+					action1 = None
+					action2 = None
+
+					#get command from program1
+					if i < len(program1):
+						action1 = program1[i]
+
+					#get command from program2
+					if i < len(program2):
+						action2 = program2[i]
+					
 
 
-		# elif self.game_state == Resources.state['BOARD']:
-		# 	print "BOARD STATE"
+					# #compare priorities and do both commands
+					# if action1 != None and action2 != None:
+					# 	#do both
+					# elif action1 != None:
+					# 	#do action1
+					# else:
+					# 	#do action2
+
+
+
+
 
 
 
